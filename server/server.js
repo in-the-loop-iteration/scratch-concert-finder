402a4b4021a4e3b44c5e19ebaab6316e6f413e93
dyeoman2 <dyeoman2@gmail.com>
2021-06-24 14:18:27 -0700

    env files infrastructure

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const config = require('./config');
 
-
-require('dotenv').config();
+const { port } = config;
 
 app.use('/build', express.static(path.resolve(__dirname, '../build')));
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
+  res.status(200).sendFile(path.resolve(__dirname, '../index.html'));
 });
 
-app.listen(3000);
+app.listen(port);
