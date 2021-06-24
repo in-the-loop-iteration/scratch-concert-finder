acb6be19471f1e4fc0771fcbaca1a91415f4adb6
keflynn <flynn.kelseyelizabeth@gmail.com>
2021-06-23 21:05:20 -0400

    Skeleton app with webpack

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = (props) => {
+
+    return (
+        <div>
+            <h2>this is our wicked awesome app</h2>
+        </div>
+    );
+}
+
+export default App;
