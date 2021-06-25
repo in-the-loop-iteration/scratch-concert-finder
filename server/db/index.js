59a2e23948e4884ee5d94361de6cf869e3a9840c
3LD3ST <71085901+3LD3ST@users.noreply.github.com>
2021-06-24 22:19:34 -0400

    worked on auth model

---
+++ server/db/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+const { database } = config;
+
+mongoose
+  .connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'inTheLoop',
+  })
+  .then(() => console.log('Connected to Mongo DB.'))
+  .catch(err => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true}
+  });
+
+// TO DO: ENCRYPTION LOGIC
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+    User,
+};
