0809c93f2b941ad032c1b0d19a91804b209b6ba1
dyeoman2 <dyeoman2@gmail.com>
2021-06-26 11:28:38 -0700

    react router setup and webpack regenerator runtime fix

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import Search from './Search';
+import Profile from './Profile';
 
 const App = () => (
   <ChakraProvider>
-    <Search />
+    <Router>
+      <Switch>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   </ChakraProvider>
 );
 
---
+++ client/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Center, Text } from '@chakra-ui/react';
+
+const Profile = () => (
+  <div>
+    <Center>
+      <Text bgGradient="linear(to-l, #7928CA,#FF0080)" bgClip="text" fontSize="6xl" fontWeight="extrabold">
+        Welcome to the Profile Page
+      </Text>
+    </Center>
+  </div>
+);
+
+export default Profile;
