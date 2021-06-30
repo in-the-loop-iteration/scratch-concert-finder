23156073e88b08e97d80e9dabcdf417c0b257726
iankjudd <iankimjudd@gmail.com>
2021-06-29 22:49:48 -0400

    spotify player works

--- server/services/spotifyAccessToken.js
+++ server/services/spotifyAccessToken.js
@@ -10,7 +10,7 @@ const spotifyAccessToken = async (name) => {
   try {
     // Lookup token information in db PENDING TABLE SETUP
     const spotifyToken =
-      'BQDyPwanBbsczG8Iu-t2l61mmyH9KzlAJ5baYETYsxgOovXbGb7DgDvqfZGmSWUf8LVIkWRRMNskvs7HnaI';
+      'BQBDnqoNNv09570dKlwl-zZATUdjys4IleL97H55sjla8wKB1k71csDScOb7fug-EVGH0w9CYXcPk2M7XiJQCM32cjzwuRkd9LkV331G0D1DyRgDEuXMO_60eZmPiJouTxtao0nS4hhxIzhJcDANrEz4VoD59hKrmmImSlJOu--hgkRpZWXeZLyctWB3e2c&refresh_token=AQCHjz6yX-Pw-SS0pkk7Ae9jmVhiVe0Dndhp9mXGf7Qkxuzj7f3qzMY7Fu0H7s9953t5gMBMSUnPYqehbU9H2iIu7p8w2CAjkPEe6xoXwH5Tli352Pmz7qRiVZIPq6RVsdE';
     const spotifyTokenGeneratedAt = moment();
     // If token hasn't expired (1 hour), return the token
     if (spotifyToken && moment() <= spotifyTokenGeneratedAt.add(1, 'hour')) return spotifyToken;
@@ -18,7 +18,8 @@ const spotifyAccessToken = async (name) => {
     const encodedIdAndSecret = Buffer.from(`${spotifyClientId}:${spotifyClientSecret}`).toString(
       'base64'
     );
-    const data = qs.stringify({ grant_type: 'client_credentials' });
+    var scope = 'user-read-private user-read-email user-read-playback-state streaming user-modify-playback-state';
+    const data = qs.stringify({ grant_type: 'client_credentials', scope: scope });
     const config = {
       method: 'post',
       url: 'https://accounts.spotify.com/api/token/',
