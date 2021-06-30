bc3c461fff03e63a074ec0849ca5a8e075296340
keflynn <flynn.kelseyelizabeth@gmail.com>
2021-06-29 21:51:24 -0400

    Commit needed before merging

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Search from './Search';
+import Profile from './Profile';
 
-const App = (props) => {
+const theme = extendTheme({});
 
-    return (
-        <div>
-            <h2>this is our way wicked awesome app</h2>
-        </div>
-    );
-}
+const App = () => (
+  <ChakraProvider theme={theme}>
+    <Router>
+      <Switch>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  </ChakraProvider>
+);
 
 export default App;
