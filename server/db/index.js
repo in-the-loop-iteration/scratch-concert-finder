1176b12fa125bb48f40922b363765b5696dafaf3
3LD3ST <71085901+3LD3ST@users.noreply.github.com>
2021-07-03 13:05:48 -0400

    added Frodo phodo

--- server/db/index.js
+++ server/db/index.js
@@ -17,6 +17,7 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  image: {type: String, required: false },
 });
 
 const tokenSchema = new Schema({
