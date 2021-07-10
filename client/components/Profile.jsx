7d1da9b9e3ab58fb4f45fde1725299b06801b710
Kailee Pedersen <kaileepedersen@Kailees-Mac-mini.local>
2021-07-10 17:18:43 -0400

    login and signup working, no user data at the moment

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -3,14 +3,17 @@ import { Avatar, Box, Button, Flex, InputGroup, Text, Input, InputRightElement }
 
 const Profile = () => {
 
+  //state that decides which view the user sees in the drawer
   const [userView, setUserView] = useState({
     loginView: true,
     createUserView: false,
     userAccountView: false
   })
 
+  //state for showing/hiding password
   const [show, setShow] = useState(false);
 
+  //state for signup info
   const [signup, setSignup] = useState({
     firstName: '',
     lastName: '',
@@ -18,14 +21,21 @@ const Profile = () => {
     password: '',
   })
 
+  //state for login info
   const [login, setLogin] = useState({
+    email: '',
+    password: ''
+  })
+
+  //state for user info
+  const [userInfo, setUserInfo] = useState({
 
   })
 
-  console.log(signup);
-
+  //controls whether password text is displayed or not
   const handleClickShowPassword = () => setShow(!show)
 
+  //links signup inputs with local state
   const handleChangeSignup = (prop) => (event) => {
     setSignup({
       ...signup,
@@ -33,10 +43,87 @@ const Profile = () => {
     });
   }
 
+  //links login inputs with local state
+  const handleChangeLogin = (prop) => (event) => {
+    setLogin({
+      ...login,
+      [prop]: event.target.value
+    })
+  }
+
+  //submits signup information to backend
+  const handleClickSignup = () => {
+    fetch('/user/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON',
+      },
+      body: JSON.stringify(signup),
+    })
+      .then(res => res.json())
+      .then(res => console.log('signup res: ', res))
+  }
+
+  const handleClickLogin = () => {
+    fetch('/user/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'Application/JSON',
+      },
+      body: JSON.stringify(login),
+    })
+      .then(res => res.json())
+      .then(res => console.log('signup res: ', res))
+  }
+
+  //submits login information to backend
 
   return (
     <Flex flexDirection="column" h="100%" w="25%">
       <Flex marginTop={10}>
+      { userView.loginView && (
+          <Box ml="3">
+            <Input 
+              mb="2"
+              width="250"
+              placeholder="Email"
+              isRequired={true}
+              value={login.email}
+              onChange={handleChangeLogin('email')} />
+            <InputGroup>
+              <Input 
+                mb="2"
+                width="250"
+                type={show ? "text" : "password"}
+                placeholder="Password"
+                isRequired={true}
+                value={login.password}
+                onChange={handleChangeLogin('password')} />
+                <InputRightElement mr="2" width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleClickShowPassword}>
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+            </InputGroup>
+            <Button
+              mb="2"
+              onClick={handleClickLogin}>
+              Login
+            </Button>
+            <br />
+            <Button
+              onClick={(prevState) => setUserView({
+                ...prevState,
+                createUserView: true,
+                loginView: false,
+              })}
+              variant="link" 
+              size="sm">
+              New user? Click to create an account.
+            </Button>
+          </Box>
+        )}
+
         { userView.createUserView && (
           <Box ml="3">
             <Input 
@@ -69,14 +156,15 @@ const Profile = () => {
                 isRequired={true}
                 value={signup.password}
                 onChange={handleChangeSignup('password')} />
-                <InputRightElement width="4.5rem">
+                <InputRightElement mr="6" width="4.5rem">
                   <Button h="1.75rem" size="sm" onClick={handleClickShowPassword}>
                     {show ? "Hide" : "Show"}
                   </Button>
                 </InputRightElement>
             </InputGroup>
             <Button
-              mb="2">
+              mb="2"
+              onClick={handleClickSignup}>
               Create Account
             </Button>
             <Button
@@ -88,34 +176,6 @@ const Profile = () => {
               variant="link" 
               size="sm">
               Already have an account? Click to login.
-            </Button>
-          </Box>
-        )}
-
-        { userView.loginView && (
-          <Box ml="3">
-            <Input 
-              mb="2"
-              width="250"
-              placeholder="Username" />
-            <Input 
-              mb="2"
-              width="250"
-              placeholder="Password" />
-            <Button
-              mb="2">
-              Login
-            </Button>
-            <br />
-            <Button
-              onClick={(prevState) => setUserView({
-                ...prevState,
-                createUserView: true,
-                loginView: false,
-              })}
-              variant="link" 
-              size="sm">
-              New user? Click to create an account.
             </Button>
           </Box>
         )}
