174aeed1d4127e136c7412e819720518c43d9323
kenchlo99 <kenchlo@comcast.net>
2021-07-14 20:45:18 -0700

    replace HQ API with ticketmaster API to search for concerts

--- server/services/getPlaylist.js
+++ server/services/getPlaylist.js
@@ -10,66 +10,56 @@ const { youtubeApiKey, ticketmasterApiKey } = require('../config');
 const getPlaylist = async ({ placeId }) => {
   try {
     const coordinates = await googleMapsPlaceLatLong(placeId);
-    console.log(coordinates);
-    // { lat: 40.7595044, lng: -73.9271644 }
-    const config1 = {
+    const config = {
       method: 'get',
-      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketmasterApiKey}&latlong=${coordinates.lat},${-coordinates.lng}&radius=50&unit=miles&classificationId=KZFzniwnSyZfZ7v7nJ`,
+      url: `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketmasterApiKey}&latlong=${coordinates.lat},${-coordinates.lng}&radius=100&unit=miles&classificationId=KZFzniwnSyZfZ7v7nJ`,
     };
-    const ticketMasterSearchResults = await axios(config1).then((response) => {
-      console.log(response.data);
-      return response.data;
+    const ticketmasterSearchResults = await axios(config).then((response) => {
+      return ('_embedded' in response.data) ? response.data._embedded.events : [];
     });
-    console.log(JSON.stringify(ticketMasterSearchResults, null, 2));
-    console.log(ticketMasterSearchResults._embedded.events.slice(0,2));
-    let concerts = ticketMasterSearchResults._embedded.events.map(concert => {
-      const { name, url, distance, dates} = concert;
-      console.log(dates);
+    let concerts = ticketmasterSearchResults.map(concert => {
+      const { name, url, distance, dates, _embedded } = concert;
       return {
-        name: title,
+        title: name,
         url,
         distance,
-        start: dates.start.localDate
+        start: dates.start.localDate,
+        venue: _embedded.venues[0].name
       }
-    })
-
-
+    });
     // let concerts = (await predictHQConcerts(coordinates));
-    // concerts = concerts.slice(0,2);
+    concerts = concerts.slice(0,2);
     console.log(JSON.stringify(concerts, null, 2));
     // const spotifyToken = await spotifyAccessToken();
     const playlist = await Promise.all(
       concerts.map(async (concert) => {
-        const { entities, location, start, end, title } = concert;
-        if (!entities || entities.length === 0) return;
-        const venue = entities[0];
+        const { venue, start, title, distance, url } = concert;
+        // if (!entities || entities.length === 0) return;
+        // const venue = entities[0];
         const titleScrubbed = title.replace(/[^\w\s]/gi, '').replace(/\s/g, '+');
         console.log(titleScrubbed);
-        const distance = await googleMapsDistance({
-          pointA: placeId,
-          pointB: venue.formatted_address,
-        });
-
-        /* to work on */
+        // const distance = await googleMapsDistance({
+        //   pointA: placeId,
+        //   pointB: venue.formatted_address,
+        // });
         const config = {
           method: 'get',
-          // url: `https://www.googleapis.com/youtube/v3/search?key=${youtubeApiKey}&part=snippet&q="${titleScrubbed}"+concert&order=relevance&type=video&videoDefinition=high&videoEmbeddable=true&topicId=/m/04rlf`,
-          url: '',
+          url: `https://www.googleapis.com/youtube/v3/search?key=${youtubeApiKey}&part=snippet&q="${titleScrubbed}"+concert&order=relevance&type=video&videoDefinition=high&videoEmbeddable=true&topicId=/m/04rlf`,
         };
         const youtubeSearchResults = await axios(config).then((response) => {
-          console.log(response.data);
+          // console.log(response.data);
           return response.data.items[0].id.videoId
         });
         const payload = {
           title: title.replace(/[^\w\s]/gi, ''),
           videoId: youtubeSearchResults,
-          venue: venue.name,
-          address: venue.formatted_address,
-          location,
+          venue,
+          // address: venue.formatted_address,
+          // location,
           start,
-          end,
+          // end,
           distance,
-          ticketsLink: `https://www.google.com/search?q=${titleScrubbed}+tickets`,
+          ticketsLink: url,
         };
         // const artistSearchResults = await spotifyArtistSearch({
         //   title: titleScrubbed,
@@ -114,7 +104,7 @@ const getPlaylist = async ({ placeId }) => {
         //     ticketPriceRange: [],
         //     ticketsLink: `https://www.google.com/search?q=${title}+tickets`,
         //   })) || [];
-        console.log(payload);
+        // console.log(payload);
         return payload;
       })
     );
